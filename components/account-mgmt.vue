ec468231069ead565f1b928eee31c14835b0a952
Brayden <bcowell04@gmail.com>
2024-05-20 07:13:08 +1000

    nearly have new account creation working

--- components/account-mgmt.vue
+++ components/account-mgmt.vue
@@ -1,8 +1,8 @@
 <template>
-    <div v-if="isLoading" class="pl-72">
+  <div v-if="isLoading" class="pl-72">
         <loading-indicator :Message="message" />
     </div>
-    <div v-else>
+  <div v-else>
     <header-text headerText="Your Profile" subHeaderText="Update details and shopping lists to help our grocers understand your needs."/>
     <form style="padding: 50px; background-color: white; border-radius: 10px; box-shadow: 1px 5px 8px grey; margin-left: 350px; margin-right: 50px;">
       <div class="space-y-12">
@@ -330,42 +330,66 @@
     <form style="padding: 50px; background-color: white; border-radius: 10px; box-shadow: 1px 5px 8px grey; margin-left: 350px; margin-right: 50px;">
       <div class="space-y-12">
         <div class="border-b border-gray-900/10 pb-12">
-          <h2 class="text-4xl font-semibold leading-7 text-gray-900">Account Management</h2><br>
-          <p class="mt-1 text-sm leading-6 text-gray-600">This information will be used for your user account / profile.</p>
+          <h2 class="text-4xl font-semibold leading-7 text-gray-900">Account Creator</h2><br>
+          <p class="mt-1 text-sm leading-6 text-gray-600">Enter Information Below To Create New User Accounts</p>
     
           <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div class="sm:col-span-4">
-              <label for="userId" class="block text-sm font-medium leading-6 text-gray-900">User ID</label>
+            <div class="sm:col-span-3">
+              <label for="familyId" class="block text-sm font-medium leading-6 text-gray-900">Family ID</label>
               <div class="mt-2">
-                <input v-model="userId" style="padding-left: 10px" disabled type="text" name="userId" id="userId" autocomplete="userId" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-deep-purple-accent-400 sm:text-sm sm:leading-6" />
+                <input v-model="familyId" style="padding-left: 10px" disabled type="text" name="familyId" id="familyId" autocomplete="familyId" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-deep-purple-accent-400 sm:text-sm sm:leading-6" />
               </div>
             </div>
-    
+
+            <div class="sm:col-span-3">
+              <div class="mt-2">
+              <label for="newUserType" class="block text-sm font-medium leading-6 text-gray-900">Account Type</label>
+              <select v-model="newUserType" required id="newUserType" name="newUserType" autocomplete="newUserType" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-deep-purple-accent-400 sm:text-sm sm:leading-6">
+                <option>Adult User</option>
+                <option>Child User</option>
+              </select>
+              </div>
+            </div>
+
             <div class="sm:col-span-3">
               <label for="firstName" class="block text-sm font-medium leading-6 text-gray-900">First name</label>
               <div class="mt-2">
-                <input v-model="firstName" style="padding-left: 10px" type="text" name="firstName" id="firstName" autocomplete="firstName" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-deep-purple-accent-400 sm:text-sm sm:leading-6" />
+                <input v-model="newFirstName" required style="padding-left: 10px" type="text" name="firstName" id="firstName" autocomplete="firstName" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-deep-purple-accent-400 sm:text-sm sm:leading-6" />
               </div>
             </div>
     
             <div class="sm:col-span-3">
               <label for="surname" class="block text-sm font-medium leading-6 text-gray-900">Last name</label>
               <div class="mt-2">
-                <input v-model="surname" style="padding-left: 10px" type="text" name="surname" id="surname" autocomplete="surname" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-deep-purple-accent-400 sm:text-sm sm:leading-6" />
+                <input v-model="newSurname" required style="padding-left: 10px" type="text" name="surname" id="surname" autocomplete="surname" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-deep-purple-accent-400 sm:text-sm sm:leading-6" />
               </div>
             </div>
     
-            <div class="sm:col-span-6">
+            <div class="sm:col-span-3">
               <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
               <div class="mt-2">
-                <input v-model="email" style="padding-left: 10px" id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-deep-purple-accent-400 sm:text-sm sm:leading-6" />
+                <input v-model="newEmail" required style="padding-left: 10px" id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-deep-purple-accent-400 sm:text-sm sm:leading-6" />
+              </div>
+            </div>
+
+            <div class="sm:col-span-3">
+              <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+              <div class="mt-2">
+                <input v-model="newPassword" required style="padding-left: 10px" id="password" name="password" type="password" autocomplete="password" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-deep-purple-accent-400 sm:text-sm sm:leading-6" />
               </div>
             </div>
           </div>
         </div>
       </div>
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+        <form style="width: 100%;" @submit.prevent="setNewUserData">
+          <button type="submit" class="rounded-md bg-blue-400 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        </form>
+      </div>
+
     </form>
 
+   
 
 
 
@@ -387,9 +411,11 @@
     const postalCode = ref('');
     const purchases = ref('');
     const shipping = ref('');
-    const stores = ref('');
+    const stores = ref(''); 
+    const familyId = ref('');
     const isLoading = ref(true);
-    const message = ref('Collecting user information...')
+    const message = ref('Collecting user information...');
+   
     
     async function getUserData () {
       try {
@@ -401,6 +427,19 @@
         return data;
       } catch (error) {
         console.error('Error in getUserData:', error)
+      }
+    }
+
+    async function getFamilyData () {
+      try {
+        const response = await fetch(`https://localhost:7249/api/Family/GetFamilyId/`+ sessionStorage.getItem('334_group_user_userId'), {
+          credentials: 'include'
+        });
+        const data = await response.json();
+        isLoading.value = false;
+        return data;
+      } catch (error) {
+        console.error('Error in getFamilyData:', error)
       }
     }
     
@@ -436,11 +475,12 @@
         console.error('Error in setUserData:', error);
         throw error;
       }
-    }
+    }    
     
     (async () => {
-      try {
+      try {        
         const userData = await getUserData();
+        const familyData = await getFamilyData();
         userId.value = userData.id;
         firstName.value = userData.givenName;
         surname.value = userData.surname;
@@ -450,12 +490,13 @@
         region.value = userData.state;
         postalCode.value = userData.postalCode;
         country.value = userData.country;
-        purchases.value = userData.purchaseNotifications;
-        shipping.value = userData.shippingNotifications;
-        stores.value = userData.storeNotifications;
+        familyId.value = familyData.id;
+
       } catch (error) {
         console.error('Error fetching user data:', error);
       }
     })();
+
+
     
     </script>
